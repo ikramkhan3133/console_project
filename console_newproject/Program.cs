f218e00e16c36c6921551bfab6e44427805cef53
ikramkhan <khanikram3133@yahoo.com>
2017-09-14 16:06:11 -0700

    Added

---
+++ console_newproject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_newproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+        }
+    }
+}
